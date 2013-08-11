1736630d49df128d6157eb644948fc02e4420416
Brendan Robert <brendan.robert@gmail.com>
2013-08-11 10:20:19 -0500

    adding pre-built assets

---
+++ build/classes-generated/org/badvision/hint/Bundle.java
@@ -0,0 +1,20 @@
+package org.badvision.hint;
+/** Localizable strings for {@link org.badvision.hint}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>Anti-aliasing</i>
+     * @see RenderingOptionsPanelController
+     */
+    static String AdvancedOption_DisplayName_Rendering() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "AdvancedOption_DisplayName_Rendering");
+    }
+    /**
+     * @return <i>Hint Antialiasing Render</i>
+     * @see RenderingOptionsPanelController
+     */
+    static String AdvancedOption_Keywords_Rendering() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "AdvancedOption_Keywords_Rendering");
+    }
+    private void Bundle() {}
+}
